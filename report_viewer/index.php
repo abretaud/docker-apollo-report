00ae665470e6e8505a69ec1b5f27f1259aa6a5e7
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2019-10-25 12:37:52 +0200

    fix

--- report_viewer/index.php
+++ report_viewer/index.php
@@ -76,7 +76,7 @@ $org_id = str_replace(' ', '_', $organism);
         <li><a href="./report_data/<?php echo $org_id."/valid.gff"; ?>">GFF3 of valid genes</a> (<a href="./report_data/<?php echo $org_id."/valid_transcripts.fa"; ?>">transcripts FASTA</a>, <a href="./report_data/<?php echo $org_id."/valid_cds.fa"; ?>">CDS FASTA</a>, <a href="./report_data/<?php echo $org_id."/valid_proteins.fa"; ?>">proteins FASTA</a>)</li>
         <li><a href="./report_data/<?php echo $org_id."/invalid.gff"; ?>">GFF3 of invalid genes</a></li>
         <li><a href="./report_data/<?php echo $org_id."/deleted.tsv"; ?>">List of genes marked as deleted</a></li>
-        <li><a href="./report_data/<?php echo $org_id."/by_group/"; ?>">GFF3 and FASTA for each groups</a></li>
+        <li><a href="./report_data/<?php echo $org_id."/by_groups/"; ?>">GFF3 and FASTA for each groups</a></li>
     </ul>
 <?php endif; ?>
 
